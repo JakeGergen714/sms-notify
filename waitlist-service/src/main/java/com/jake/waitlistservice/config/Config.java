a1d6f40e11f8be6138c9098daaf9a44bbb6c2b5e
jake gergen <jake.gergen714@gmail.com>
2023-12-27 10:11:58 -0500

    waitlist-service refactored coors policy

--- waitlist-service/src/main/java/com/jake/waitlistservice/config/Config.java
+++ waitlist-service/src/main/java/com/jake/waitlistservice/config/Config.java
@@ -14,7 +14,7 @@ public class Config implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/localhost:3000").allowedOrigins("*")
                         .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowCredentials(true);
             }
         };
