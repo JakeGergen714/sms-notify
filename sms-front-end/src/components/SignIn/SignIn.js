267cdd4fa6fc5e2880eb5f8d1eede96b0907041f
jake gergen <jake.gergen714@gmail.com>
2023-12-27 13:28:54 -0500

    stylings typo

--- sms-front-end/src/components/SignIn/SignIn.js
+++ sms-front-end/src/components/SignIn/SignIn.js
@@ -27,7 +27,9 @@ const SignIn = () => {
    return (
       <div className='signin-background d-flex align-items-center justify-content-center'>
          <div className='signin-card p-3'>
-            <h1 className='text-center mb-4'>The WaitList</h1>
+            <h1 id='login-title' className='text-center mb-4'>
+               The WaitList
+            </h1>
             <form onSubmit={handleLogin}>
                <div className='mb-3'>
                   <input
