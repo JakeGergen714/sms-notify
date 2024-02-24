a0ea81d193b82278aaf6c423a6cde7d7ebcd1047
jake gergen <jake.gergen714@gmail.com>
2024-02-24 12:13:51 -0500

    default user

--- auth/src/main/java/com/jake/auth/service/JpaUserDetailsService.java
+++ auth/src/main/java/com/jake/auth/service/JpaUserDetailsService.java
@@ -5,11 +5,13 @@ import com.jake.auth.jpa.domain.UserRole;
 import com.jake.auth.jpa.repo.UserRepository;
 import com.jake.auth.jpa.repo.UserRoleRepository;
 import com.jake.auth.userdetails.UserDetailsImpl;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,16 +22,32 @@ import java.util.List;
 public class JpaUserDetailsService implements UserDetailsService {
     private final UserRepository userRepo;
     private final UserRoleRepository roleRepo;
+    private final PasswordEncoder encoder;
+
+    @PostConstruct
+    public void t() {
+        if(userRepo.findByUsername("test") == null ) {
+            User user = new User();
+            user.setUsername("test");
+            user.setPassword(encoder.encode("test"));
+            user.setId(5L);
+            userRepo.save(user);
+        }
+        
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("Looking user up by username <{}>", username);
         User user = userRepo.findByUsername(username);
-        List<UserRole> roles = roleRepo.findAllByUserId(user.getId());
 
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
+
+        List<UserRole> roles = roleRepo.findAllByUserId(user.getId());
+
+
 
         return new UserDetailsImpl(user, roles);
     }
