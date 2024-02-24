66db61ee85d4b2ee5289795d7d0b8a762d3795d8
jake gergen <jake.gergen714@gmail.com>
2024-02-24 13:12:20 -0500

    refactor waitlist service to be oauth2 resource server

--- waitlist-service/src/main/java/com/jake/waitlistservice/controller/Controller.java
+++ waitlist-service/src/main/java/com/jake/waitlistservice/controller/Controller.java
@@ -1,21 +1,17 @@
 package com.jake.waitlistservice.controller;
 
-import com.jake.waitlistservice.dto.AuthorizationDTO;
 import com.jake.waitlistservice.dto.WaitListItemDTO;
 import com.jake.waitlistservice.jpa.domain.WaitListItem;
-import com.jake.waitlistservice.service.AuthService;
 import com.jake.waitlistservice.service.WaitListService;
-import com.jake.waitlistservice.util.JwtUtil;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -25,24 +21,20 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class Controller {
     private final WaitListService service;
-    private final AuthService authService;
 
     @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true") // Replace with your allowed origin
     @GetMapping(value="/waitList")
-    public ResponseEntity<List<WaitListItem>> getAllWaitListItems(@RequestHeader String accessToken) {
+    public ResponseEntity<List<WaitListItem>> getAllWaitListItems(Authentication authenticationToken) {
+        Jwt jwt = (Jwt)authenticationToken.getPrincipal();
+        log.info(jwt.getClaims());
+      /*  String username = jwt.getClaimAsString("username");
         log.info("getAllWaitListItems <{}>", accessToken);
 
-        if(!authService.validateJwt(accessToken)) {
-            log.info("Validation Failed");
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
         log.info("Validation Successful");
 
-        AuthorizationDTO authorizationDTO = JwtUtil.decodeJwt(accessToken);
-        log.info("Authorization DTO <{}>", authorizationDTO);
         List<WaitListItem> items = service.findAllForUser(authorizationDTO.getUsername());
-        log.info("Found Items <{}>", items);
-        return ResponseEntity.ok(items);
+        log.info("Found Items <{}>", items);*/
+        return ResponseEntity.ok(List.of(new WaitListItem()));
     }
 
     @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true") // Replace with your allowed origin
@@ -50,23 +42,12 @@ public class Controller {
     public ResponseEntity<HttpStatus> addWaitListItem(@RequestHeader String accessToken, @RequestBody WaitListItemDTO waitListItemDTO) {
         log.info("addWaitListItem <{}>, <{}>", accessToken, waitListItemDTO);
 
-        /*log.info("addWaitListItem <{}>", waitListItemDTO);
-        Optional<Cookie> accessTokenCookie = getAccessTokenCookie(httpRequest.getCookies());
-        if(accessTokenCookie.isEmpty()) {
-            log.error("post /waitlist no access token found");
-            return;
-        }*/
-        if(!authService.validateJwt(accessToken)) {
-            log.info("Validation Failed");
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
         log.info("Validation Successful");
 
-        AuthorizationDTO authorizationDTO = JwtUtil.decodeJwt(accessToken);
-        log.info("Authorization DTO <{}>", authorizationDTO);
 
-        log.info("Adding waitlist item for username <{}>", authorizationDTO.getUsername());
-        service.add(waitListItemDTO, authorizationDTO.getUsername());
+
+        //log.info("Adding waitlist item for username <{}>", authorizationDTO.getUsername());
+        //service.add(waitListItemDTO, authorizationDTO.getUsername());
 
         return ResponseEntity.ok().build();
     }
