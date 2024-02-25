8d53efee54462554c3103b5ed92ae6f7225cd11a
jake gergen <jake.gergen714@gmail.com>
2024-02-24 20:10:22 -0500

    added xsrf token as cookie in gateway

---
+++ basic-cloud-gateway/src/main/java/com/jake/basiccloudgateway/config/CsrfCookieWebFilter.java
@@ -0,0 +1,37 @@
+package com.jake.basiccloudgateway.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.web.server.Cookie;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Component
+@Log4j2
+public class CsrfCookieWebFilter implements WebFilter {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String key = CsrfToken.class.getName();
+
+        Mono<CsrfToken> csrfToken = exchange.getAttribute(key);
+        if (csrfToken != null) {
+            log.info("ADDING COOKIE");
+            return csrfToken.doOnSuccess(token -> {
+                ResponseCookie cookie = ResponseCookie.from("XSRF-TOKEN", token.getToken())
+                        .maxAge(Duration.ofHours(1)).httpOnly(false).path("/").sameSite(Cookie.SameSite.LAX.attributeValue()).build();
+                exchange.getResponse().getCookies().add("XSRF-TOKEN", cookie);
+            }).then(chain.filter(exchange));
+        } else {
+            log.info("NOT ADDING COOKIE");
+            return Mono.empty();
+        }
+    }
+}
