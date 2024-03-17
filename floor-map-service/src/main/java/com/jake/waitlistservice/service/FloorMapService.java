a6fe50e7d2efd1536c674f8b16c56c27789691a9
jake gergen <jake.gergen714@gmail.com>
2024-03-17 15:48:15 -0400

    mroe logging

--- floor-map-service/src/main/java/com/jake/waitlistservice/service/FloorMapService.java
+++ floor-map-service/src/main/java/com/jake/waitlistservice/service/FloorMapService.java
@@ -7,6 +7,7 @@ import com.jake.waitlistservice.jpa.domain.FloorMapItem;
 import com.jake.waitlistservice.jpa.repository.FloorMapItemRepository;
 import com.jake.waitlistservice.jpa.repository.FloorMapRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import java.math.BigInteger;
@@ -15,6 +16,7 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
+@Log4j2
 public class FloorMapService {
     private final FloorMapItemRepository repo;
     private final FloorMapRepository floorMapRepository;
@@ -27,14 +29,18 @@ public class FloorMapService {
         Optional<FloorMap> floorMapOptional = floorMapRepository.findByName(floorMapDTO.getName());
         FloorMap floorMap;
         if(floorMapOptional.isEmpty()) {
+            log.info("No existing Floor Plan found.");
             floorMap = new FloorMap();
             floorMap.setName(floorMapDTO.getName());
             floorMap.setBusinessId(businessId);
             FloorMap saved =  floorMapRepository.save(floorMap);
         } else {
             FloorMap existing = floorMapOptional.get();
+            log.info("Found existing Floor Plan <{}>.", existing);
             existing.setName(floorMapDTO.getName());
+            log.info("Edited existing Floor Plan <{}>.", existing);
             floorMap = floorMapRepository.save(existing);
+            log.info("Saved Floor Plan <{}>.", existing);
         }
 
         return floorMap;
