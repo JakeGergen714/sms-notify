9de3a9c0c4ca9a51083e9d1441ae6271041d8c47
jake gergen <jake.gergen714@gmail.com>
2024-05-05 16:44:23 -0400

    added scan for data lib

--- restaurant-service/src/main/java/com/jake/restaurantservice/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/jake/restaurantservice/RestaurantServiceApplication.java
@@ -2,8 +2,12 @@ package com.jake.restaurantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories({"com.jake.datacorelib"})
+@EntityScan({"com.jake.datacorelib"})
 public class RestaurantServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantServiceApplication.class, args);
