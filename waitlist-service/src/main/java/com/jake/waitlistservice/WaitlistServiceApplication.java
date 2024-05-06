2eed932e7423de439e5571908cac4fa7017e9727
jake gergen <jake.gergen714@gmail.com>
2024-05-06 14:16:40 -0400

    fixed waitlist entity scan

--- waitlist-service/src/main/java/com/jake/waitlistservice/WaitlistServiceApplication.java
+++ waitlist-service/src/main/java/com/jake/waitlistservice/WaitlistServiceApplication.java
@@ -2,10 +2,11 @@ package com.jake.waitlistservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories({"com.jake.datacorelib.waitlist"})
+@EntityScan({"com.jake.datacorelib.waitlist"})
 @SpringBootApplication
 public class WaitlistServiceApplication {
 	public static void main(String[] args) {
