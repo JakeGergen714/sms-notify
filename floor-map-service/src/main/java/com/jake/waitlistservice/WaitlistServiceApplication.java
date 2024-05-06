9f9273822095c71340d88eb13c7442c27173f8e8
jake gergen <jake.gergen714@gmail.com>
2024-05-06 13:46:25 -0400

    more bug fixes

--- floor-map-service/src/main/java/com/jake/waitlistservice/WaitlistServiceApplication.java
+++ floor-map-service/src/main/java/com/jake/waitlistservice/WaitlistServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories({"com.jake.datacorelib.floormap"})
-@EntityScan({"com.jake.datacorelib.floormap", "com.jake.datacorelib.servicetype"})
+@EntityScan({"com.jake.datacorelib.floormap", "com.jake.datacorelib.restaurant", "com.jake.datacorelib.servicetype", "com.jake.datacorelib.serviceschedule"})
 public class WaitlistServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WaitlistServiceApplication.class, args);
