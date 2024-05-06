71dcd2e5e258ba975f3df61620fc914ae2947c22
jake gergen <jake.gergen714@gmail.com>
2024-05-06 13:42:31 -0400

    bug fixes

--- floor-map-service/src/main/java/com/jake/waitlistservice/WaitlistServiceApplication.java
+++ floor-map-service/src/main/java/com/jake/waitlistservice/WaitlistServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories({"com.jake.datacorelib.floormap"})
-@EntityScan({"com.jake.datacorelib.floormap"})
+@EntityScan({"com.jake.datacorelib.floormap", "com.jake.datacorelib.servicetype"})
 public class WaitlistServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WaitlistServiceApplication.class, args);
--- restaurant-service/src/main/java/com/jake/restaurantservice/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/jake/restaurantservice/RestaurantServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories({"com.jake.datacorelib.restaurant", "com.jake.datacorelib.servicetype", "com.jake.datacorelib.serviceschedule"})
+@EnableJpaRepositories({"com.jake.datacorelib.restaurant", "com.jake.datacorelib.servicetype", "com.jake.datacorelib.serviceschedule", "com.jake.datacorelib.floormap"})
 @EntityScan({"com.jake.datacorelib.restaurant", "com.jake.datacorelib.servicetype", "com.jake.datacorelib.serviceschedule", "com.jake.datacorelib.floormap"})
 public class RestaurantServiceApplication {
 	public static void main(String[] args) {
