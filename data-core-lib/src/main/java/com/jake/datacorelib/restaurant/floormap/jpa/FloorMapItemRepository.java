dcc4fe15233336f8b63d518d06ee5704f09abd20
jake gergen <jake.gergen714@gmail.com>
2024-05-07 22:02:19 -0400

    fixed repo after entity refactor

--- data-core-lib/src/main/java/com/jake/datacorelib/restaurant/floormap/jpa/FloorMapItemRepository.java
+++ data-core-lib/src/main/java/com/jake/datacorelib/restaurant/floormap/jpa/FloorMapItemRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FloorMapItemRepository extends JpaRepository<FloorMapItem, Long> {
-    List<FloorMapItem> findAllByFloorMapId(long floorMapId, Sort sortedBy);
 
-    List<FloorMapItem> findAllByFloorMapId(long floorMapId);
 }
